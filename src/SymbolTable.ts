6a59e4964e6d4c742807f258c8a87f77ecd7fe3a
Stevan Little <stevan.little@gmail.com>
2025-07-11 16:07:35 +0200

    initial commit

---
+++ src/SymbolTable.ts
@@ -0,0 +1,146 @@
+
+type Identifier = string // [A-Za-z_][A-Za-z0-9_]+
+
+type Undef = { type : 'UNDEF' }
+type True  = { type : 'TRUE'  }
+type False = { type : 'FALSE' }
+
+type IV = { type : 'INT', value : number }
+type NV = { type : 'NUM', value : number }
+type PV = { type : 'STR', value : string }
+
+type SV =
+    | Undef
+    | True
+    | False
+    | IV
+    | NV
+    | PV
+    | RV
+
+type AV = { type : 'LIST', value : SV[] }
+type HV = { type : 'HASH', value : Map<string, SV> }
+type CV = { type : 'CODE' } // TODO
+type RV = { type : 'REF', value : SV | AV | HV | CV | RV }
+
+type Glob = {
+    type  : 'GLOB',
+    name  : Identifier,
+    slots : {
+        '$' : SV | Undef,
+        '@' : AV | Undef,
+        '%' : HV | Undef,
+        '&' : CV | Undef,
+    }
+}
+
+type Stash = {
+    type  : 'STASH',
+    name  : Identifier,
+    stash : Map<Identifier, GV>,
+}
+
+type GV = Glob | Stash
+
+type Any = SV | AV | HV | CV | GV
+
+// -----------------------------------------------------------------------------
+
+const SV_Undef : Undef = { type : 'UNDEF' }
+const SV_True  : True  = { type : 'TRUE'  }
+const SV_False : False = { type : 'FALSE' }
+
+function isUndef (sv : Any) : sv is Undef { return sv.type == 'UNDEF' }
+function isTrue  (sv : Any) : sv is True  { return sv.type == 'TRUE'  }
+function isFalse (sv : Any) : sv is False { return sv.type == 'FALSE' }
+
+function assertIsUndef (sv : Any) : asserts sv is Undef {
+    if (isUndef(sv)) throw new Error(`Not Undef ??(${JSON.stringify(sv)})`)
+}
+
+function assertIsTrue (sv : Any) : asserts sv is True {
+    if (isTrue(sv)) throw new Error(`Not True ??(${JSON.stringify(sv)})`)
+}
+
+function assertIsFalse (sv : Any) : asserts sv is False {
+    if (isFalse(sv)) throw new Error(`Not False ??(${JSON.stringify(sv)})`)
+}
+
+// -----------------------------------------------------------------------------
+
+function newIV (value : number) : IV { return { type : 'INT', value } }
+function newNV (value : number) : NV { return { type : 'NUM', value } }
+function newPV (value : string) : PV { return { type : 'STR', value } }
+
+function isIV (sv : Any) : sv is IV { return sv.type == 'INT' }
+function isNV (sv : Any) : sv is NV { return sv.type == 'NUM' }
+function isPV (sv : Any) : sv is PV { return sv.type == 'STR' }
+
+function assertIsIV (sv : Any) : asserts sv is IV {
+    if (isIV(sv)) throw new Error(`Not IV ??(${JSON.stringify(sv)})`)
+}
+
+function assertIsNV (sv : Any) : asserts sv is NV {
+    if (isNV(sv)) throw new Error(`Not NV ??(${JSON.stringify(sv)})`)
+}
+
+function assertIsPV (sv : Any) : asserts sv is PV {
+    if (isPV(sv)) throw new Error(`Not PV ??(${JSON.stringify(sv)})`)
+}
+
+function IVtoNV (iv : IV) : NV { return newNV(iv.value) }
+function IVtoPV (iv : IV) : PV { return newPV(String(iv.value)) }
+
+function NVtoIV (nv : NV) : IV { return newIV(Math.trunc(nv.value)) }
+function NVtoPV (nv : NV) : PV { return newPV(String(nv.value)) }
+
+function PVtoIV (pv : PV) : IV { return newIV(Number.parseInt(pv.value)) }
+function PVtoNV (pv : PV) : NV { return newNV(Number.parseFloat(pv.value)) }
+
+// -----------------------------------------------------------------------------
+
+function isSV (sv : Any) : sv is SV {
+    return isUndef(sv) || isTrue(sv) || isFalse(sv)
+        || isIV(sv)    || isNV(sv)   || isPV(sv);
+}
+
+function assertIsSV (sv : Any) : asserts sv is SV {
+    if (isSV(sv)) throw new Error(`Not SV ??(${JSON.stringify(sv)})`)
+}
+
+// -----------------------------------------------------------------------------
+
+function newAV () : AV { return { type : 'LIST', value : [] } }
+
+function isAV (av : Any) : av is AV { return av.type == 'LIST' }
+
+function assertIsAV (av : Any) : asserts av is AV {
+    if (isAV(av)) throw new Error(`Not AV ??(${JSON.stringify(av)})`)
+}
+
+// -----------------------------------------------------------------------------
+
+function newHV () : HV { return { type : 'HASH', value : new Map<string, SV>() } }
+
+function isHV (hv : Any) : hv is HV { return hv.type == 'HASH' }
+
+function assertIsHV (hv : Any) : asserts hv is HV {
+    if (isHV(hv)) throw new Error(`Not HV ??(${JSON.stringify(hv)})`)
+}
+
+
+// -----------------------------------------------------------------------------
+
+function newCV () : CV { return { type : 'CODE' } }
+
+function isCV (cv : Any) : cv is CV { return cv.type == 'CODE' }
+
+function assertIsCV (cv : Any) : asserts cv is CV {
+    if (isCV(cv)) throw new Error(`Not CV ??(${JSON.stringify(cv)})`)
+}
+
+// -----------------------------------------------------------------------------
+
+
+
+
